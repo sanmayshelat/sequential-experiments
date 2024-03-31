266b076c4eac0ca3ca6d29b1db1ffcd45a3d7697
Sanmay Shelat <sanmayatwork@gmail.com>
2024-03-31 19:38:11 -0400

    vectorized power calculation

--- design.py
+++ design.py
@@ -10,54 +10,65 @@ class Sequential:
 		self.delta = delta
 
 	def fixed_sample_proportion(self) -> Tuple[int, int]:
-		# search space
-		N_max = 5000
-		d_star_max = 200
+		if self.delta <= 0.02:
+			raise NotImplementedError(
+				"""
+				Effect sizes <= 2%% are currently not supported.
+				They can be implemented by searching within a smaller
+				space of N and d_star.
+				"""
+			)
+			N_max = 300_000
+			d_star_max = 1500
+		elif self.delta <= 0.05:
+			N_max = 100_000
+			d_star_max = 500
+		elif self.delta <= 0.1:
+			N_max = 12_000
+			d_star_max = 250
+		else:
+			N_max = 3_000
+			d_star_max = 120
 
-		log_i_cumsum = np.log(range(1, N_max)).cumsum()
+		log_i_cumsum = np.log(np.arange(1, N_max + 1)).cumsum()
 		log_p_h1 = np.log(1 / (2 + self.delta))
 		log_q_h1 = np.log((1 + self.delta) / (2 + self.delta))
 		log_p_h0 = np.log(1 / 2)
 
-		flag = 0
+		d_star_range = np.arange(2, d_star_max, 2)
+		d_star_values = np.tile(d_star_range, (N_max // 2, 1)).T
+		n_values = np.tile(np.arange(2, N_max + 1, 2), (len(d_star_range), 1))
 
-		for N in range(N_max):
-			for d_star in range(2, d_star_max, 2):
-				h1 = sum(
-					np.exp(
-						np.log(d_star / n)
-						+ log_i_cumsum[n - 1]
-						- log_i_cumsum[max(int((n - d_star) / 2) - 1, 0)]
-						- log_i_cumsum[int((n + d_star) / 2) - 1]
-						+ ((n - d_star) / 2) * log_p_h1
-						+ ((n + d_star) / 2) * log_q_h1
-					)
-					for n in range(2, N + 1, 2)
-				)
-				if h1 <= 1 - self.beta:
-					break
+		n_d_star_2_minus = (n_values - d_star_values) // 2
+		n_d_star_2_minus_max = np.maximum(n_d_star_2_minus - 1, 0)
 
-				h0 = sum(
-					np.exp(
-						np.log(d_star / n)
-						+ log_i_cumsum[n - 1]
-						- log_i_cumsum[max(int((n - d_star) / 2) - 1, 0)]
-						- log_i_cumsum[int((n + d_star) / 2) - 1]
-						+ n * log_p_h0
-					)
-					for n in range(2, N + 1, 2)
-				)
-				if (h1 > 1 - self.beta) & (h0 < self.alpha):
-					flag = 1
-					break
+		n_d_star_2_plus = (n_values + d_star_values) // 2
+		n_d_star_2_plus = n_d_star_2_plus - 1
 
-			if flag == 1:
-				break
-		print(
-			f"""
-            P(H0):{h0}
-            P(H1):{h1}
-            N:{N}, d*: {d_star}
-        """
+		h1_terms = np.exp(
+			np.log(d_star_values / n_values)
+			+ log_i_cumsum[n_values - 1]
+			- log_i_cumsum[n_d_star_2_minus_max]
+			- log_i_cumsum[n_d_star_2_plus]
+			+ (n_d_star_2_minus) * log_p_h1
+			+ (n_d_star_2_plus + 1) * log_q_h1
 		)
+		h1 = np.cumsum(h1_terms, axis=1)
+
+		h0_terms = np.exp(
+			np.log(d_star_values / n_values)
+			+ log_i_cumsum[n_values - 1]
+			- log_i_cumsum[n_d_star_2_minus_max]
+			- log_i_cumsum[n_d_star_2_plus]
+			+ n_values * log_p_h0
+		)
+		h0 = np.cumsum(h0_terms, axis=1)
+
+		d_star, N = np.unravel_index(
+			np.argmax((h0 < self.alpha) & (h1 > (1 - self.beta))), h1.shape
+		)
+		N = (N + 1) * 2
+		d_star = (d_star + 1) * 2
+
+		print(f"""N:{N}, d*: {d_star}""")
 		return N, d_star
